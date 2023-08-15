d35d234a9f1c176ee547c4c2a2303c6a93a3e571
bmiller180787 <bmiller180787@gmail.com>
2023-08-15 19:20:12 +0100

    starting set-up

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+function Footer () {
+    return (
+        <div className="footer">
+            <p>Created by Brent Miller</p>
+            <p>bmiller180787</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+function Header () {
+    return(
+        <div>
+            <h1>
+                Stock Manager
+            </h1>
+        </div>
+    )
+}
+
+export default Header;
