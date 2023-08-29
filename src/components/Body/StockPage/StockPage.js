b3e72cf1bb901be47841ff4e6bd1f317e336f240
bmiller180787 <bmiller180787@gmail.com>
2023-08-29 19:18:02 +0100

    added state

--- src/components/Body/StockPage/StockPage.js
+++ src/components/Body/StockPage/StockPage.js
@@ -1,9 +1,23 @@
 import "./StockPage.css"
+import {useEffect, useState} from "react";
+import {json} from "react-router-dom";
 
-function StockPage () {
+function StockPage() {
+
+    const [items, setItems] = useState("")
+
+    useEffect(() => {
+        fetch("")
+            .then((data => data.json())
+                .then((data) => {
+                    setItems(data)
+                })
+            )
+    }, [])
+
     return (
         <>
-            This is the Stock page
+
         </>
     )
 }
